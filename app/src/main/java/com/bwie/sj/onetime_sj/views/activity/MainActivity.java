c99e2cf407b137479212e332a5bb9cc34e78f689
CrysSun <2697538638@qq.com>
2018-03-09 20:32:18 +0800

    git的测试

--- app/src/main/java/com/bwie/sj/onetime_sj/views/activity/MainActivity.java
+++ app/src/main/java/com/bwie/sj/onetime_sj/views/activity/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试--
+        //和好覅是否合适
     }
 }
